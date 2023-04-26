de4763c1013a33371f0ac5c5f909e1c0d2f4a214
Truite Séchée <66438253+Truiteseche@users.noreply.github.com>
2023-04-26 13:23:46 +0200

    Update commitAndPushSomething.bat
    Change file path "User" hard value by %USERPROFILE%

--- commitAndPushSomething.bat
+++ commitAndPushSomething.bat
@@ -1,7 +1,7 @@
 @echo off
-cd C:/Users/Mael/Desktop/autoGitRecipient
+cd %USERPROFILE%/Desktop/autoGitRecipient
 for %%i in (1 2 3 4 5 6 7 8) do (
-	echo %random% %%i>C:/Users/Mael/Desktop/autoGitRecipient/file.txt
+	echo %random% %%i>%USERPROFILE%/Desktop/autoGitRecipient/file.txt
 	git add .
 	git commit -m "Commit from autoGitCommiter"
 	git push -u origin main
